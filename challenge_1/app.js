91e42120047f50c0b57abba2c361ae9d6623560a
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-02-12 12:10:46 -0800

    Renders board, toggles color on click

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,12 @@
 
 
+var cell = document.getElementsByTagName("td");
+// document.getElementsByTagName("td").setattribute
+
+function red(obj) {
+  if (obj.className !== "selected") {
+    obj.setAttribute("class", "selected");
+  } else {
+    obj.setAttribute("class", "");
+  }
+}
