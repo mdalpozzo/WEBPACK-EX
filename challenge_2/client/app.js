3cb71b4522d120f598be2ca51c8914bd569a68e1
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-02-14 09:11:57 -0800

    need to make parser nicer and clean up xmlhttprequest

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,3 +1,41 @@
+//click handler for form and button
+document.getElementById("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  var inputText = document.getElementById("input").value;
+  submitCSV(inputText);
+}, false)
+
 function submitCSV (form) {
-  var JSONdata = document.getElementById("input").value;
+  // var JSONstring = document.getElementById("input").value;
+  // console.log(form);
+  // $.get('/', )
+  // $.ajax({
+  //   type: 'POST',
+  //   url: '/',
+  //   data: JSONstring,
+  //   success: (err) => {
+  //     console.log('SOMETHING\'S WRONG... it\'s probably you.);
+  //   },
+  //   dataType: 'application/json'
+  // });
+
+  let response = new XMLHttpRequest;
+  response.open('POST', '/convert');
+  response.setRequestHeader('Content-Type','application/json');
+  response.onreadystatechange = function () {
+    if (response.readystate === 4) {
+      console.log('hello');
+      let response = new XMLHttpRequest;
+      response.open('GET', '/convert');
+      response.setRequestHeader('Content-Type', 'text/csv');
+      //onload -- event listener for successful completion of xmlhttprequest transaction
+      response.onload = function () {
+        let CSVdata = response.responseText;
+        console.log(CSVdata);
+        //attach to dom
+      }
+    }
+  }
+  response.send(form);
+  console.log(response);
 }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var json2csv = require('express-json-2-csv');
 var bodyParser = require('body-parser');
+
+var convertedJSON;
+//helper functions
+
+var json2csv = function (obj) {
+  var csv = [];
+  var keys = Object.keys(obj).slice(0, -1);
+  csv.push(keys);
+
+  var parser = function (obj) {
+    var values = [];
+    for (var i = 0; i < keys.length; i++) {
+      values.push(obj[keys[i]]);
+    }
+    csv.push(values);
+
+    if (obj.children !== undefined) {
+      for (var i = 0; i < obj.children.length; i++) {
+        parser(obj.children[i]);
+      }
+    }
+  }
+
+  parser(obj);
+  
+  return csv;
+}
+
+
 //configure app
   // app.set('view', path.join(__dirname, 'view'));
 
@@ -11,18 +39,35 @@ app.use(express.static('client'));
 app.use(bodyParser.json());  //??????????????????
 //define routes
 
-app.get('/', function (req, res) {
-  res.sendFile(client/index.html);
+//next??? 
+app.get('/', function (req, res, next) {
+  res.sendFile('client/index.html');
+  next();
 });
 
-app.post('', function (req, res) {
+//recieves post request when form is submitted
+app.post('/convert', function (req, res, next) {
+  //req.body has JSONtext from form input
+  //PARSE JSON TO CSV RIGHT HERE-ISH
+  convertedJSON = json2csv(req.body);
+  // res.setHeader
 
+  // req.body
+  res.send(convertedJSON);
+  // next();
+});
+
+app.get('/convert', function (req, res, next) {
+  console.log(convertedJSON);
+  res.send(convertedJSON);
 });
 
 var port = 1337;
 app.listen(port, function () {
-  console.log(`listening on port: ${port}`)
+  console.log(`listening on port: ${port}`);
 });
+
+
 
 
 
